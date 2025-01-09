b9174ff4dd0438b2c51ba48be2e4e198a8ea97de
dev1abhi <abhilashsarangi222@gmail.com>
2025-01-09 20:48:08 +0530

    add MongoDB connection and update dependencies in package.json

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+});
 
 app.use(express.json());
 
